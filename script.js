2c01dbaa94505b780496834eff99e1c5e64f28cd
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-08-29 19:03:24 +0300

    commit

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var message = "Hello from external JavaScript!";
-alert(message);
+// var message = "Hello from external JavaScript!";
+// alert(message);
+var header= document.querySelectorAll("#header1")
+console.log(header)
+var header= document.querySelectorAll(".header1")
+console.log(header)
+// header.style.color="red"
